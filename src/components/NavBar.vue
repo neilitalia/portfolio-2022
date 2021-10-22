252f575e5351c809a1d7ab15a0b85d77df362d30
Neil Italia <neilitalia1998@gmail.com>
2021-10-22 17:41:05 -0500

    :lipstick: apply tailwind classes

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="nav">
+  <div id="nav" class="flex flex-row bg-red-500">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
