d6d2365c75078da6c552958b097fe627266fc006
Neil Italia <neilitalia1998@gmail.com>
2021-10-22 17:39:15 -0500

    :lipstick: add tailwind colors library

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: { content: ["./public/**/*.html", "./src/**/*.vue"] },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors
   },
   variants: {
     extend: {},
