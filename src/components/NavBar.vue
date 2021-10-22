6db37a7453e3b18124e819985f5ba11633e8eb17
Neil Italia <neilitalia1998@gmail.com>
2021-10-22 18:02:59 -0500

    :sparkles: add navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,27 @@
 <template>
-  <div id="nav" class="flex flex-row bg-red-500">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div
+    id="nav"
+    class="
+      flex flex-row
+      bg-emerald-500
+      h-7
+      items-center
+      justify-between
+      text-xl
+    "
+  >
+    <div>
+      <h1 class="text-coolGray-900 font-bold">Neil Italia</h1>
+    </div>
+    <div class="text-coolGray-900">
+      <router-link to="/" class="text-coolGray-900 hover:text-coolGray-100"
+        >Home</router-link
+      >
+      |
+      <router-link to="/about" class="text-coolGray-900 hover:text-coolGray-100"
+        >About</router-link
+      >
+    </div>
   </div>
 </template>
 
@@ -10,6 +30,3 @@ export default {
   name: "NavBar",
 };
 </script>
-
-<style>
-</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   purge: { content: ["./public/**/*.html", "./src/**/*.vue"] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      textColor: ['visited'],
+      backgroundColor: ['active'],
+      fontFamily: {
+        sans: ['Jost', ...defaultTheme.fontFamily.sans],
+      },
+    },
     colors
   },
   variants: {
