ab40dba441b23a4161e3499e0e8eae2cb576938c
Neil Italia <neilitalia1998@gmail.com>
2021-10-23 13:36:09 -0500

    :heavy_plus_sign: add boxicons dependency

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "boxicons";
+import "boxicons/css/boxicons.min.css";
 import "./assets/tailwind.css";
 
 Vue.config.productionTip = false;
