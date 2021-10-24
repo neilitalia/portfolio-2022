e3ab9e25ca2f15dac318dd554503b2a5eeed44eb
Neil Italia <neilitalia1998@gmail.com>
2021-10-23 22:17:45 -0500

    :construction: store project information in json file

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,8 +1,9 @@
 <template>
   <div
     class="
-      cursor-pointer
+      flex flex-col
       border-2 border-emerald-400 border-opacity-50
+      bg-white
       rounded-xl
       my-3
       transform
@@ -12,18 +13,44 @@
       ease-in-out
     "
   >
-    <img src="https://placebear.com/640/360" alt="" class="rounded-t-lg" />
-    <h1 class="text-amber-100 text-lg">Card Title</h1>
-    <h3 class="text-amber-100 text-lg">Card Description</h3>
-    <div class="flex flex-row justify-start items-center">
-      <p class="text-amber-100 text-lg mr-5">
-        <i class="bx bxl-github"></i>
-        Source Code
-      </p>
-      <p class="text-amber-100 text-lg">
-        <i class="bx bx-link-external"></i>
-        Live Demo
-      </p>
+    <div>
+      <img src="https://placebear.com/640/360" alt="" class="rounded-t-lg" />
+    </div>
+    <div class="p-5 text-emerald-900">
+      <h1 class="text-xl font-bold">Card Title</h1>
+      <h3 class="text-lg">Card Description</h3>
+      <div class="flex flex-row justify-start items-center">
+        <a
+          :href="'#'"
+          target="_blank"
+          class="
+            text-lg
+            mr-5
+            hover:text-emerald-600
+            transition
+            duration-150
+            ease-in-out
+          "
+        >
+          <i class="bx bxl-github"></i>
+          Source Code
+        </a>
+        <a
+          :href="'#'"
+          target="_blank"
+          class="
+            text-lg
+            mr-5
+            hover:text-emerald-600
+            transition
+            duration-150
+            ease-in-out
+          "
+        >
+          <i class="bx bx-link-external"></i>
+          Live Demo
+        </a>
+      </div>
     </div>
   </div>
 </template>
