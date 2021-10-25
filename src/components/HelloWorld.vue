d2b3112943856016f91bdc271feca67b2badb277
Neil Italia <neilitalia1998@gmail.com>
2021-10-24 19:44:37 -0500

    :sparkles: add contact details

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,20 +11,6 @@
       </h3>
       <h1 class="text-5xl font-bold text-amber-100 underline">Neil Italia.</h1>
       <p id="typeit" class="text-xl lg:text-2xl text-amber-400 w-72 mt-5"></p>
-      <!-- <div class="flex flex-row justify-start items-center text-3xl mt-16">
-        <a href="https://github.com/neilitalia" target="_blank">
-          <i class="bx bxl-github"></i>
-        </a>
-        <a href="https://www.linkedin.com/in/neilitalia/" target="_blank">
-          <i class="bx bxl-linkedin-square"></i>
-        </a>
-        <a href="https://github.com/neilitalia" target="_blank">
-          <i class="bx bx-mail-send"></i>
-        </a>
-        <a href="https://github.com/neilitalia" target="_blank">
-          <i class="bx bx-phone"></i>
-        </a>
-      </div> -->
     </div>
   </div>
 </template>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -9,15 +9,58 @@
     }"
   >
     <h1 class="text-5xl text-amber-100 font-bold mt-24">Contact</h1>
-    <p>{{ text }}</p>
+    <div
+      class="
+        text-amber-100
+        flex flex-col
+        lg:flex-row
+        justify-center
+        items-center
+      "
+    >
+      <div class="flex flex-col justify-start items-center lg:h-full">
+        <h3 class="text-center text-2xl lg:text-2xl font-medium mt-10 lg:mt-24">
+          Let's work on something cool together.
+        </h3>
+        <div class="flex flex-row justify-start items-center text-2xl mt-16">
+          <a href="https://github.com/neilitalia" target="_blank" class="mx-3">
+            <i class="bx bxl-github"></i>
+            GitHub
+          </a>
+          <a
+            href="https://www.linkedin.com/in/neilitalia/"
+            target="_blank"
+            class="mx-3"
+          >
+            <i class="bx bxl-linkedin-square"></i>
+            LinkedIn
+          </a>
+          <a href="mailto:hello@neilitalia.dev" class="mx-3">
+            <i class="bx bx-mail-send"></i>
+            Email
+          </a>
+          <a href="tel:817-751-2462" class="mx-3">
+            <i class="bx bx-phone"></i>
+            Phone
+          </a>
+        </div>
+      </div>
+      <CollabIllustration
+        class="mb-10 mt-16 lg:w-3/6 lg:mt-0 lg:mr-10 lg:mb-10 md:w-96"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import Vue from "vue";
+import CollabIllustration from "@/assets/CollabIllustration.vue";
 
 export default Vue.extend({
   name: "Contact",
+  components: {
+    CollabIllustration,
+  },
   props: ["intersectionOptions"],
   data: function () {
     return {
